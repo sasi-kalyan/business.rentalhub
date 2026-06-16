b673cb87029e0ff1c70145fd769b0c79030c553a
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 21:20:15 +0530

    application-entry point

---
+++ src/main/java/com/business/rentalhub/RentalhubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.business.rentalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalhubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalhubServiceApplication.class, args);
+	}
+
+}
