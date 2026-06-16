75178f66c494ce4150e7767cb82669ab2e649582
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 21:21:02 +0530

    created property-repo

---
+++ src/main/java/com/business/rentalhub/repository/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.business.rentalhub.repository;
+
+import com.business.rentalhub.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+}
