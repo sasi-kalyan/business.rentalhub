0594b57bab7e40422393a13913539f596beea61e
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 21:22:48 +0530

    misc

---
+++ src/test/java/com/business/rentalhub/RentalhubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.rentalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalhubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
